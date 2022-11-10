1fad2a2b26d45c272180a10a38b4f522be7a2f8e
Legatus727 <trobbins428@gmail.com>
2022-11-10 23:12:50 +0000

    Loop Challenges

---
+++ loopChallenges/rangeChallenge.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+for i in reversed(range(100)):
+    print(str(i) + " bottles of beer on the wall!")
+    print(str(i) + " bottles of beer on the wall! " + str(i) + " bottles of beer! You take one down, pass it around")
+
